6f66d89f4a0a10b1593c754f77509be0e75a6adc
Isabela Migliorini <isabelamigli@gmail.com>
2024-06-06 17:26:45 -0300

    criação de classe tarefa e classe lista de tarefas. teste main.

---
+++ collections-java-api/src/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,18 @@
+package list.operacoesBasicas;
+
+public class Tarefa {
+	
+	// atributo
+	private String descricao;
+
+	// construtor
+	public Tarefa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	// getter
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
