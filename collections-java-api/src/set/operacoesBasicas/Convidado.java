5d8fe0c5643be9f2e43d2b5224dd2f900e8c560a
Isabela Migliorini <isabelamigli@gmail.com>
2024-06-08 14:31:28 -0300

    métodos hashcode e equals + método main para testes

--- collections-java-api/src/set/operacoesBasicas/Convidado.java
+++ collections-java-api/src/set/operacoesBasicas/Convidado.java
@@ -1,5 +1,7 @@
 package set.operacoesBasicas;
 
+import java.util.Objects;
+
 public class Convidado {
 	
 	private String nome;
@@ -30,6 +32,23 @@ public class Convidado {
 	@Override
 	public String toString() {
 		return "Convidado [nome=" + nome + ", codigoConvite=" + codigoConvite + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigoConvite);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Convidado other = (Convidado) obj;
+		return codigoConvite == other.codigoConvite;
 	}
 
 }
